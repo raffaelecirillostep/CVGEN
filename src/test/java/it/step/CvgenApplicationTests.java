3b5784ea5337c472faa59715869ec7a7fe1a5889
raffaele_cirillo <raffaele.cirillo@stepsrl.it>
2024-03-22 17:08:25 +0100

    initial commit

---
+++ src/test/java/it/step/CvgenApplicationTests.java
@@ -0,0 +1,13 @@
+package it.step;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvgenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
